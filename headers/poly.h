4c5524672d9a869abce20122eee49c5e419aaece
Jérem SP <jeremie.spiesser@gmail.com>
2020-11-04 12:02:15 +0100

    updated headers polgen -> poly

---
+++ headers/poly.h
@@ -0,0 +1,24 @@
+//
+// Created by Nitorac on 16/09/2020.
+//
+
+#ifndef SCHRODINGER_IPS_POLGEN_H
+#define SCHRODINGER_IPS_POLGEN_H
+
+#include <armadillo>
+
+/**
+ * Poly class
+ */
+class Poly
+{
+public:
+    // Hermite related functions
+    void calcHermite(int,arma::vec);
+    arma::vec hermite(int);
+    // Laguerre related functions
+    void calcLaguerre(int,int,arma::vec);
+    arma::vec laguerre(int,int);
+};
+
+#endif //SCHRODINGER_IPS_POLGEN_H
