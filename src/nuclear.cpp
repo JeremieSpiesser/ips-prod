5dc9128036409b8a1b0a0e6dc5daa3373ddece25
Jérem SP <jeremie.spiesser@gmail.com>
2020-11-25 11:20:36 +0100

    added first sum optimization

--- src/nuclear.cpp
+++ src/nuclear.cpp
@@ -40,3 +40,32 @@ Nuclear::naiveCalc(){
     }
     return result;
 }
+
+arma::mat
+Nuclear::opti1Calc(){
+    arma::mat result = arma::zeros(r.n_elem, z.n_elem); // number of points on r- and z- axes
+    for (int m = 0; m < basis.mMax; m++)
+    {
+        for (int n = 0; n < basis.nMax(m); n++)
+        {
+            arma::vec rpart1 = basis.rPart(r,m,n);
+            for (int n_z = 0; n_z < basis.n_zMax(m, n); n_z++)
+            {
+                arma::vec zpart1 = basis.zPart(z,n_z) ;
+
+                for (int np = 0; np < basis.nMax(m); np++)
+                {
+                    for (int n_zp = 0; n_zp < basis.n_zMax(m, np); n_zp++)
+                    {
+                        //arma::mat funcA = basis.basisFunc( m,  n,  n_z, zVals, rVals);
+                        arma::mat funcA = zpart1 * rpart1.t();
+                        //arma::mat funcB = basis.basisFunc(m, np, n_zp, zVals, rVals);
+                        arma::mat funcB = basis.zPart(z,n_zp) * basis.rPart(r,m,np).t();
+                        result += funcA % funcB * rho(m, n, n_z, m , np, n_zp); // mat += mat % mat * double
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
